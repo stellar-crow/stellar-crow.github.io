1c4cedd8958be17434fbbe323537517b80c41f9c
stellar-crow <29088200+stellar-crow@users.noreply.github.com>
2024-12-18 20:28:12 +0300

    change title Explorer

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -10,6 +10,7 @@ import { i18n } from "../i18n"
 
 // Options interface defined in `ExplorerNode` to avoid circular dependency
 const defaultOptions = {
+  title: "Lore",
   /*folderClickBehavior: "collapse",*/
   folderClickBehavior: "link",
   folderDefaultState: "collapsed",
